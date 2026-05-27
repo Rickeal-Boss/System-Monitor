c3063ddaf9c9af92ea9b55dc4313cade43b00184
RB Station <15992567646@139.com>
2026-05-27 16:33:28 +0800

    Delete java/com/example/deviceinfoviewer directory

--- java/com/example/deviceinfoviewer/data/model/HistoryDataPoint.kt
+++
@@ -1,7 +0,0 @@
-package com.example.deviceinfoviewer.data.model
-
-data class HistoryDataPoint(
-    var timestampMillis: Long = 0L,
-    var value: Float = 0f,
-    var seriesName: String? = null
-)
--- java/com/example/deviceinfoviewer/data/model/MobileNetworkInfo.kt
+++
@@ -1,9 +0,0 @@
-package com.example.deviceinfoviewer.data.model
-
-data class MobileNetworkInfo(
-    var networkType: String = "",
-    var operatorName: String = "",
-    var mccMnc: String = "",
-    var signalStrengthDbm: Int = Int.MIN_VALUE,
-    var isRoaming: Boolean = false
-)
